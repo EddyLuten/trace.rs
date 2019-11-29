7d5b1a2731be03104c78a5492436e3505fff1bb7
Eddy Luten <eddyluten@gmail.com>
2019-11-28 17:49:49 -0800

    Shadows and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# trace.rs
+
+A very naive ray tracing implementation made as an exercise in order to get
+better at Rust.
+
+Produces this image as a PPM, though it's easily modified to produce others:
+
+![spheres](spheres.png)
--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,10 @@ struct Ray {
 }
 
 impl Vector3D {
+  fn clone(&self) -> Vector3D {
+    Vector3D { x: self.x, y: self.y, z: self.z }
+  }
+
   fn mul(&self, scalar: &f32) -> Vector3D {
     Vector3D { x: self.x * scalar, y: self.y * scalar, z: self.z * scalar }
   }
@@ -49,6 +53,14 @@ impl Vector3D {
     return Vector3D { x: self.x * s, y: self.y * s, z: self.z * s};
   }
 
+  fn clamp(&self, min: f32, max: f32) -> Vector3D {
+    Vector3D {
+      x: self.x.min(max).max(min),
+      y: self.y.min(max).max(min),
+      z: self.z.min(max).max(min),
+    }
+  }
+
   fn rgb(&self) -> [u8; 3] {
     return [
       (self.x * 255.0) as u8,
@@ -67,9 +79,7 @@ impl Sphere {
     let sr2 = self.radius * self.radius;
     let d2 = l2oc - (tca * tca);
 
-    if d2 > sr2 {
-      return None;
-    }
+    if d2 > sr2 { return None; }
 
     let thc = (sr2 - d2).sqrt();
     let t0 = tca - thc;
@@ -91,15 +101,12 @@ impl Sphere {
   }
 }
 
-impl Light {
-}
-
 fn main() -> std::io::Result<()> {
-  const WIDTH: usize = 800;
-  const HEIGHT: usize = 600;
-  const F_WIDTH: f32 = WIDTH as f32;
-  const F_HEIGHT: f32 = HEIGHT as f32;
-  const ASPECT: f32 = F_WIDTH / F_HEIGHT;
+  const WIDTH:    usize = 800;
+  const HEIGHT:   usize = 600;
+  const F_WIDTH:  f32   = WIDTH as f32;
+  const F_HEIGHT: f32   = HEIGHT as f32;
+  const ASPECT:   f32   = F_WIDTH / F_HEIGHT;
 
   let mut pixels: Vec<u8> = Vec::with_capacity(WIDTH * HEIGHT * 3);
 
@@ -107,17 +114,27 @@ fn main() -> std::io::Result<()> {
     Sphere {
       position: Vector3D { x: 0.0, y: 0.0, z: -5.0 },
       color: Vector3D { x: 1.0, y: 0.0, z: 0.0 },
-      radius:   1.0
+      radius:   1.0,
     },
     Sphere {
-      position: Vector3D { x: 1.0, y: 0.1, z: -3.0 },
+      position: Vector3D { x: 0.5, y: 0.1, z: -3.0 },
       color: Vector3D { x: 0.0, y: 0.0, z: 1.0 },
-      radius:   0.1
+      radius:   0.1,
     },
     Sphere {
-      position: Vector3D { x: -1.0, y: 0.1, z: -3.0 },
+      position: Vector3D { x: -0.5, y: 0.1, z: -3.0 },
       color: Vector3D { x: 0.0, y: 1.0, z: 0.0 },
-      radius:   0.1
+      radius:   0.1,
+    },
+    Sphere {
+      position: Vector3D { x: 0.0, y: 0.5, z: -3.0 },
+      color: Vector3D { x: 1.0, y: 1.0, z: 1.0 },
+      radius:   0.1,
+    },
+    Sphere {
+      position: Vector3D { x: 0.0, y: -0.5, z: -3.0 },
+      color: Vector3D { x: 0.3, y: 0.3, z: 0.3 },
+      radius:   0.1,
     }
   ];
 
@@ -127,38 +144,51 @@ fn main() -> std::io::Result<()> {
   };
 
   let lights = [
-     Light {
-      direction: Vector3D { x: -2.0, y: 10.0, z: -3.0 },
-      intensity: 1.0,
+      Light {
+      direction: Vector3D { x: 0.0, y: 0.0, z: -4.0 },
+      intensity: 0.1,
     },
-    // opposite angle
     Light {
-      direction: Vector3D { x: 2.0, y: -10.0, z: -3.0 },
-      intensity: 0.2,
+      direction: Vector3D { x: 0.0, y: -0.5, z: -4.0 },
+      intensity: 0.1,
     },
   ];
 
   for y in 0..HEIGHT {
     for x in 0..WIDTH {
       let mut pixel = [0, 0, 0];
+
       let rx = (((x as f32 + 0.5) / F_WIDTH) * 2.0 - 1.0) * ASPECT;
       let ry = 1.0 - ((y as f32 + 0.5) / F_HEIGHT) * 2.0;
+
       ray.direction = (Vector3D { x: rx, y: ry, z: -3.0 }).normalize();
 
       for sphere in &spheres {
         match sphere.intersects(&ray) {
           Some(distance) => {
             if distance >= 0.0 {
-              let hit_point   = ray.origin.add(&ray.direction.mul(&distance));
-              let normal      = sphere.surface_normal(&hit_point);
+              let hit_point = ray.origin.add(&ray.direction.mul(&distance));
+              let normal    = sphere.surface_normal(&hit_point);
 
               let mut light_power = 0.0;
               for light in &lights {
-                let light_dir   = light.direction.normalize().mul(&-1.0);
-                light_power += normal.dot(&light_dir).max(0.0) * light.intensity;
+                let light_dir  = light.direction.normalize().mul(&-1.0);
+                let shadow_ray = Ray {
+                  // this feels like an anti-pattern...
+                  origin:    hit_point.clone(),
+                  direction: light_dir.clone(),
+                };
+
+                let light_intensity: f32 = spheres
+                  .iter()
+                  .map(|s| if s.intersects(&shadow_ray).is_none() { light.intensity } else { 0.0 })
+                  .sum();
+
+                light_power += normal.dot(&light_dir).max(0.0) * light_intensity;
               }
 
-              pixel = sphere.color.mul(&light_power).rgb();
+              // clamping to 0->1 is insufficient for lights brighter than 1.0
+              pixel = sphere.color.mul(&light_power).clamp(0.0, 1.0).rgb();
             }
           },
           None => {} // keep the pixel
